61b9f4197557fcc123db7d14744508324277f25d
abhi9720 <abhishektiwari9720@gmail.com>
2021-04-09 21:21:16 +0530

    changing sessio logic

--- app.js
+++ app.js
@@ -7,32 +7,33 @@ if(process.env.NODE_ENV !="production{"){
 
 //console.log(process.env.SECRET);
 
-const express = require('express');
-
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
+const express = require('express'); // v
+const path = require('path'); // v
+const mongoose = require('mongoose'); // v
+const ejsMate = require('ejs-mate'); // v
+const session = require('express-session'); // v
 const ExpressError = require('./utilities/ExpressError');
-const methodOverride = require('method-override'); // override http verb 
+const flash  =  require('connect-flash'); // v
 
-const flash  =  require('connect-flash');
+const methodOverride = require('method-override'); // override http verb  // v
 const passport  =  require('passport');;
 const LocalStrategy =  require('passport-local');
 const User  = require('./models/user')
-
+const helmet  =  require('helmet')
 const mongoSanitize = require('express-mongo-sanitize');
 
-const helmet  =  require('helmet')
 
 // requiring our router 
 const campgroundsRoutes =  require('./routes/campgrounds');
 const reviewsRoutes =  require('./routes/reviews');
 const userRoutes =  require('./routes/users');
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp' ;
+const MongoDBStore = require('connect-mongo');
+
+const dbUrl =  'mongodb://localhost:27017/yelp-camp' ;
+console.log( process.env.DB_URL);
+//   process.env.DB_URL ||
 // 'mongodb://localhost:27017/yelp-camp';
 
 
@@ -45,10 +46,11 @@ mongoose.connect(dbUrl, {
     useFindAndModify:false
 
 })
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error :')); // testing db connected or not 
 db.once('open', () => {
-    console.log('DataBase Connected ');
+    console.log('---------------DataBase Connected -------------- ');
 })
 
 const app = express();
@@ -61,90 +63,32 @@ app.set('views', path.join(__dirname, 'views')); // to void conflict if we call 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize());
-app.use(helmet({}));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
-
-
-    const scriptSrcUrls = [
-        "https://stackpath.bootstrapcdn.com",
-        "https://api.tiles.mapbox.com",
-        "https://api.mapbox.com",
-        "https://kit.fontawesome.com",
-        "https://cdnjs.cloudflare.com",
-        "https://cdn.jsdelivr.net",
-        
-
-        
-    ];
-    const styleSrcUrls = [
-        "https://kit-free.fontawesome.com",
-        "https://stackpath.bootstrapcdn.com",
-        "https://api.mapbox.com",
-        "https://api.tiles.mapbox.com",
-        
-        "https://use.fontawesome.com",
-        
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css",
-        
-        
-
-
-    ];
-    const connectSrcUrls = [
-        "https://fonts.gstatic.com",
-        "https://api.mapbox.com",
-        "https://*.tiles.mapbox.com",
-        "https://events.mapbox.com",
-    ];
-    const fontSrcUrls = [
-       
-        
-
-    ];
-    app.use(
-        helmet.contentSecurityPolicy({
-            directives: {
-                defaultSrc: [],
-                connectSrc: ["'self'", ...connectSrcUrls],
-                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-                workerSrc: ["'self'", "blob:"],
-                childSrc: ["blob:"],
-                objectSrc: [],
-                imgSrc: [
-                    "'self'",
-                    "blob:",
-                    "data:",
-                    "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                    "https://images.unsplash.com",
-                ],
-                fontSrc: ["'self'", ...fontSrcUrls],
-            },
-        })
-    );
 
     const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-    // const store = MongoStore.create({
-    //     mongoUrl: dbUrl,
-    //     secret,
-    //     touchAfter: 24 * 60 * 60
-    // });
+    const store = new MongoDBStore({
+        mongoUrl: dbUrl,
+        secret,
+        touchAfter: 24 * 60 * 60
+    });
     
-    // store.on("error", function (e) {
-    //     console.log("SESSION STORE ERROR", e)
-    // })
+    store.on("error", function (e) {
+        console.log("-----------------------SESSION STORE ERROR...............................", e)
+    })
 
 const sessionConfig = {
     secret,
-        store: MongoStore.create({ 
-            mongoUrl: dbUrl ,
-                touchAfter:24*3600,
-                secret,
+    store,
+        // store: MongoStore.create({ 
+        //     mongoUrl: dbUrl ,
+        //         touchAfter:24*3600,
+        //         secret,
 
-        }),
+        // }),
         name: 'session',
        secret,
         resave: false,
@@ -159,11 +103,69 @@ const sessionConfig = {
     
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+    
+
+    
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    
+    "https://use.fontawesome.com",
+    
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css",
+    
+    
+
+
+];
+const connectSrcUrls = [
+    "https://fonts.gstatic.com",
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [
+   
+    
+
+];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 
 // this to be before passport.session
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -172,8 +174,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
-    //console.log(req.session);
+    console.log('Session Details ............... ');
+    console.log(req.user);
   
+
     res.locals.currentUser = req.user;
     
     res.locals.success =  req.flash('success');
@@ -207,12 +211,12 @@ app.all('*', (req, res, next) => { // for any path this will run (mean for any t
 
 //defining our error  if something went wrong 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something Went Wrong ' } = err; // eefault code and default message 
+    const { statusCode = 500} = err; // eefault code and default message 
     //   res.status(statusCode).send(message+"  *****  Error Thrown *****");
     //   res.send('Ohhh Boy Something Went Wrong!!! ');
     // we can also decide on the basis of error what to show
 
-    if (!err.message || !err.message) {
+    if (!err.message) {
         err.statusCode = 500;
         err.message = 'Something Went Wrong Please again try later';
     }
